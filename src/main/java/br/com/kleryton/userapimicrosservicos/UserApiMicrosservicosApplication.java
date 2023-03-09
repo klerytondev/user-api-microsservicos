2fea89f7eef6c6191c2685895ab97270be712147
Kleryton Souza <kleryton.dev@gmail.com>
2023-03-09 12:14:55 -0300

    project created

---
+++ src/main/java/br/com/kleryton/userapimicrosservicos/UserApiMicrosservicosApplication.java
@@ -0,0 +1,13 @@
+package br.com.kleryton.userapimicrosservicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApiMicrosservicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApiMicrosservicosApplication.class, args);
+	}
+
+}
